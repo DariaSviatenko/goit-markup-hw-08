8e2136cba320f14b011091713f25855057bacba7
DariaSviatenko <svyatikdasha@gmail.com>
2022-07-29 17:54:01 +0300

    add changes

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,12 +1,16 @@
 (() => {
   const headerBtnRef = document.querySelector("[data-header__button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileMenuClose = document.querySelector("[data-menu-close]");
 
   headerBtnRef.addEventListener("click", () => {
-    const expanded = 
-    headerBtnRef.getAttribute ("aria-expanded") === "true" || false;
+    const expanded =
+      headerBtnRef.getAttribute("aria-expanded") === "true" || false;
     headerBtnRef.classList.toggle("is-open");
-    headerBtnRef.setAttribute ("aria-expanded", !expanded);
+    headerBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
+  });
+  mobileMenuClose.addEventListener("click", () => {
+    mobileMenuRef.classList.remove("is-open");
   });
 })();
